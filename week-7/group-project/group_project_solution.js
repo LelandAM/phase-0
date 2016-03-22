fe7cd602ba9b2770eae6aa8a89ff864e2ba11692
Leland Meiners <lelandam@gmail.com>
2016-03-22 00:41:09 -0500

    add 7.8

---
+++ week-7/group-project/group_project_solution.js
@@ -0,0 +1,16 @@
+// User Stories
+// --------------
+
+// I have two sets of numbers called oddLengthArray and evenLengthArray...
+
+// Story 1
+// I want to find the sum of all the numbers in each list. Call this function sum.
+
+// Story 2
+// I want to find the average of each list of numbers. Call this function mean.
+
+// Story 3
+// I want to find the median of each list of numbers. Call this function median.
+
+
+// add pseudocode and pass on without the above descriptions.
