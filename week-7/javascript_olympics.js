1f3dac5fd9ba8ed8ade6b75a2947d064764a7dfa
Leland Meiners <Leland@Lelands-iMac.local>
2016-03-22 19:05:24 -0500

    add 7.6 pairing

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,90 @@
+ // JavaScript Olympics
+
+// I paired with Kelson Adams on this challenge.
+
+// This challenge took me [1.5] hours.
+
+
+// Warm Up
+
+
+
+
+// Bulk Up
+//  var addWin = function(athletes) {
+//  	for (i in athletes){
+//  		athletes[i] = athletes[i].name +" won the "+ athletes[i].event + "!";
+//  		console.log(athletes[i]);
+//  	};
+//  	console.log(athletes);
+//  };
+
+// var sarah = {
+// 	name: "Sarah Hughes",
+// 	event: "Ladies' Singles"
+// };
+
+// addWin([sarah]);
+
+
+// Jumble your words
+
+// var jumbler = function(myString) {
+// 	console.log(myString.split("").reverse().join(""))
+// 	return myString.split("").reverse().join("")
+// }
+
+// var myString = "Jumble This"
+
+// jumbler(myString);
+
+// 2,4,6,8
+
+	// function evens(numbers){
+	// 	for (var i = 0; i < numbers.length; i++){
+	// 		if (numbers[i] % 2 == 0){
+	// 			console.log(numbers[i])
+				
+	// 		};
+	// 	}
+	// }
+	// var numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+	// evens(numbers)
+			
+
+
+// "We built this city"
+
+// function Athlete(name, age, sport, quote){
+// 	this.name = name;
+// 	this.age = age;
+// 	this.sport = sport;
+// 	this.quote = quote;
+
+// }
+
+
+// var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+// console.log(michaelPhelps.constructor === Athlete)
+// console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+
+
+// Reflection
+// What JavaScript knowledge did you solidify in this challenge?
+// This was a useful challenge for learning about the constructor method, the for loop, arrays,
+// and getting comfortable with JavaScript in general
+
+// What are constructor functions?
+// It's a function used to create an object type, it's like saving a specific set of hash keys as 
+// a template you can add values to for as many entries as you want. This will be super helpful for 
+// gathering shipping or billing info on a retail site. 
+
+// How are constructors different from Ruby classes (in your research)?
+// It seems like a constructor is more of a designated template for a specific object, especially a big
+// list with lots of fields and a ruby class is more of a giant method made of other methods, which can
+// also be a big list you can update from outside the class if the attributes are set to write. 
+
+
+
